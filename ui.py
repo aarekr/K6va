f6603d8239f76da23a77e077b9a4ee6a88f9c4e1
aarekr <aare212@hotmail.com>
2024-12-28 19:40:03 +0200

    Card images, player's two cards

--- ui.py
+++ ui.py
@@ -5,12 +5,24 @@ import random
 import pygame
 
 BOARD_SIZE = (950, 750)
+WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+GREEN = (0, 170, 0)
+
+CARDS = []
+CARD_IMAGE_SIZE = (100, 130)
+CARD_LIST = ["cards/6_of_spades.png", "cards/6_of_clubs.png", "cards/6_of_diamonds.png", "cards/6_of_hearts.png",
+            "cards/7_of_spades.png", "cards/7_of_clubs.png", "cards/7_of_diamonds.png", "cards/7_of_hearts.png"]
+for i in range(8):
+    image = pygame.image.load((CARD_LIST[i]))
+    image = pygame.transform.scale(image, CARD_IMAGE_SIZE)
+    CARDS.append(image)
+print("CARDS:", CARDS)
 
 def game_top_text():
     """ Displaying K6va text """
     text_font = pygame.font.Font(pygame.font.get_default_font(), 60)
-    game_starts = "K6va"
+    game_starts = "Kõva"
     label = text_font.render(game_starts, 0, RED)
     return label
 
@@ -23,7 +35,7 @@ class UI:
     def start_game(self):
         """ Texts when game starts """
         pygame.init()
-        self.screen.blit(game_top_text(), (250, 15))
+        self.screen.blit(game_top_text(), (370, 15))
         #self.screen.blit(game_start_text(), (210, 250))
         pygame.display.update()
         start_game = False
@@ -40,10 +52,14 @@ class UI:
     def game_loop(self):
         """ Game loop """
         pygame.init()
-        self.screen.blit(game_top_text(), (250, 15))
+        self.screen.fill(GREEN)
+        self.screen.blit(game_top_text(), (370, 15))
+        pygame.display.update()
 
         print("\nGame started")
         while self.game_active:
+            self.screen.blit(CARDS[4], (330, 600))
+            self.screen.blit(CARDS[5], (450, 600))
             pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
